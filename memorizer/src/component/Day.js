e0921bb0a971e0a7088b490b8c2030db5b7040d3
khv2644511 <khv2644511@naver.com>
2023-03-08 17:20:36 +0900

    Feat: Day 이동 버튼 추가

--- memorizer/src/component/Day.js
+++ memorizer/src/component/Day.js
@@ -1,16 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Word from "./Word";
 import useFetch from "../hooks/useFetch";
 
 export default function Day() {
   const { day } = useParams();
-
   const words = useFetch(`http://localhost:3001/words?day=${day}`);
+  const days = useFetch(`http://localhost:3001/days`);
+  const navigate = useNavigate();
+
+  function PrevDay(e) {
+    if (Number(day) > 1) navigate(`/day/${Number(day) - 1}`);
+  }
+
+  function NextDay() {
+    if (Number(day) < days.length) navigate(`/day/${Number(day) + 1}`);
+  }
 
   return (
     <>
-      <h2>Day {day}</h2>
-      {words.length === 0 && <span>Loading...</span>}
+      <button
+        className="prev_btn"
+        onClick={PrevDay}
+        style={{ opacity: Number(day) <= 1 ? 0.3 : 1 }}
+      >
+        {"<"}
+      </button>
+      <h2 style={{ display: "inline", padding: "0px 5px" }}>Day {day}</h2>
+      <button
+        onClick={NextDay}
+        style={{ opacity: Number(day) === days.length ? 0.3 : 1 }}
+      >
+        {">"}
+      </button>
+      {words.length === 0 && <div>Loading...</div>}
       <table>
         <tbody>
           {words.map((word) => (
